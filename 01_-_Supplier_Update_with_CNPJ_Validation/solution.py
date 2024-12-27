b7f61d4ac03e493fd93526759c5210f6cbf0591d
Edson Copque <ecop@disroot.org>
2024-12-27 19:19:17 -0300

    Update solution.py

--- 01_-_Supplier_Update_with_CNPJ_Validation/solution.py
+++ 01_-_Supplier_Update_with_CNPJ_Validation/solution.py
@@ -3,39 +3,39 @@
 from models import *
 from dao import *
 
-class SupplierController:
-    def change(self, nameOld, nameNew, cnpjNew, telephoneNew, categoryNew):
+def change(self, nameOld, nameNew, cnpjNew, telephoneNew, categoryNew):
         supplierdao_read = SupplierDao.read()
 
+        # supplier_name = list(filter(lambda x: x.name == nameOld, supplierdao_read)
+        #A001:
         supplier_name = []
         for i1 in supplierdao_read:
             if i1.name == nameOld:
                 supplier_name.append(i1)
 
         if len(supplier_name) > 0:
+            # supplier_cnpj = list(filter(lambda x: x.cnpj == cnpjNew, supplierdao_read))
             supplier_cnpj = []
             for i2 in supplierdao_read:
                 if i2.cnpj == cnpjNew:
                     supplier_cnpj.append(i2)
-            
+
+            # supplier_updated = list(map(lambda x: SupplierModel(nameNew, cnpjNew, telephoneNew, categoryNew) if(x.name == nameOld) else x, supplierdao_read))
             if len(supplier_cnpj) == 0:
-                supplier_final = []
-                for i3 in supplierdao_read:
-                    supplier_final.append(SupplierModel(nameNew, cnpjNew, telephoneNew, categoryNew))
-                else:
-                    supplier_final.append(i3)
+                for i3 in range(len(supplierdao_read)):
+                    if supplierdao_read[i3].name == nameOld:
+                        supplierdao_read[i3] = SupplierModel(nameNew, cnpjNew, telephoneNew, categoryNew)
+                        print(f'Supplier {nameOld} updated successfully.')
+                        break
             else:
                 print('The CNPJ already exists.')
-                supplier_final = supplierdao_read
         else:
             print('The supplier you want to change does not exists.')
-            supplier_final = supplierdao_read
-    
+
         with open('hd_supplier.txt', 'w') as file:
-            for i4 in supplier_final:
-                file.writelines(i4.name + '|' + 
-                            i4.cnpj + '|' + 
-                            i4.telephone + '|' + 
-                            i4.category)
+            for i4 in supplierdao_read:
+                file.writelines(i4.name + '|' +
+                                i4.cnpj + '|' +
+                                i4.telephone + '|' +
+                                str(i4.category))
                 file.writelines('\n')
-                print('Method executed successfully.')
