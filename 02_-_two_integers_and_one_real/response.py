3e4949759de47012bd4efc22c963ca5b76d1df09
Edson Copque <ecop@disroot.org>
2025-02-19 10:37:21 -0300

    Create response.py

---
+++ 02_-_two_integers_and_one_real/response.py
@@ -0,0 +1,11 @@
+first_number = int(input('Enter the first number: '))
+second_number = int(input('Enter the second number: '))
+third_number = float(input('Enter the third real number: '))
+
+my_product = (first_number * 2) * (second_number / 2)
+my_sum = (first_number * 3) + (third_number)
+my_third = third_number ** 3
+
+print(f'Product: {my_product}')
+print(f'Sum: {my_sum}')
+print(f'Third: {my_third:.2f}')
