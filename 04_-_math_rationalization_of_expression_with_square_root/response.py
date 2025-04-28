0569422c8de5333d137f2acd6670fdfc45ed35a7
Edson Copque <ecop@disroot.org>
2025-04-28 11:47:53 -0300

    Create response.py

---
+++ 04_-_math_rationalization_of_expression_with_square_root/response.py
@@ -0,0 +1,7 @@
+#1: Calculate √10 manually (using approximation).
+
+# A good guess for √10​ is x0=3, since √3 = 9 and √4 = 16.
+# Babylonian Method (iterative formula): xn + 1 = (xn + (S / xn) / 2).
+# 'S' is the nuymber we want the square root. S = 10.
+# Iteration (xo = 3): x1 = (x0 + (10/x0) / 2) = (3 + (10 / 3) / 2) = ((3 + 3,33333333) / 2) = (6,33333333 / 2) ≈ 3,16666667. You can continue iteration.
+# I made several iterations and the value of x4 is identical to x3, up to 8 decimal places provided, showing that the method has already converged to the desired precision. This is because √10 is very close to 3, and additional iterations do not significantly change the result within this precision. Therefore, I can create the code.
