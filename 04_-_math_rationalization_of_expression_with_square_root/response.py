1d3d5031e22ae916d50b9448163150badd1fa3e3
Edson Copque <ecop@disroot.org>
2025-04-28 12:42:18 -0300

    Update response.py

--- 04_-_math_rationalization_of_expression_with_square_root/response.py
+++ 04_-_math_rationalization_of_expression_with_square_root/response.py
@@ -5,3 +5,15 @@
 # 'S' is the nuymber we want the square root. S = 10.
 # Iteration (xo = 3): x1 = (x0 + (10/x0) / 2) = (3 + (10 / 3) / 2) = ((3 + 3,33333333) / 2) = (6,33333333 / 2) ≈ 3,16666667. You can continue iteration.
 # I made several iterations and the value of x4 is identical to x3, up to 8 decimal places provided, showing that the method has already converged to the desired precision. This is because √10 is very close to 3, and additional iterations do not significantly change the result within this precision. Therefore, I can create the code.
+
+def calculates_square_root(n, precision=0.0001):
+    estimate = (n / 2.0)
+    while True:
+        next_estimate = (((estimate + n) / estimate) / 2)
+        if abs(next_estimate - estimate) < precision:
+            return next_estimate
+        else:
+            estimate = next_estimate
+
+sqrt_10 = calculates_square_root(10)
+print(sqrt_10)
