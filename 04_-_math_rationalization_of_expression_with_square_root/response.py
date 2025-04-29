4e9a17c42231689015054ba998e6c6fba53a2dc2
Edson Copque <ecop@disroot.org>
2025-04-28 21:28:08 -0300

    Update response.py

--- 04_-_math_rationalization_of_expression_with_square_root/response.py
+++ 04_-_math_rationalization_of_expression_with_square_root/response.py
@@ -6,7 +6,6 @@
 # Iteration (xo = 3): x1 = (x0 + (10/x0) / 2) = (3 + (10 / 3) / 2) = ((3 + 3,33333333) / 2) = (6,33333333 / 2) ≈ 3,16666667. You can continue iteration.
 # I made several iterations and the value of x4 is identical to x3, up to 8 decimal places provided, showing that the method has already converged to the desired precision. This is because √10 is very close to 3, and additional iterations do not significantly change the result within this precision. Therefore, I can create the code.
 
-
 # Step 1: Calculate sqrt(10) using the Babylonian method (already implemented):
 def calculates_square_root(n, precision=0.0001):
     estimate = (n / 2.0)
@@ -26,10 +25,10 @@ print()
 
 # Step 2: Define the original expression and the conjugate
 numerator = (sqrt_10 + 3)
-numerator_str = '√10 + 3'
+numerator_str = '√10 + 3' #TODO
 
 denominator = (sqrt_10 - 3)
-denominator_str = '√10 - 3'
+denominator_str = '√10 - 3' #TODO
 
 conjugate = (sqrt_10 + 3)
 
@@ -94,5 +93,6 @@ for c in range(15, 25):
         else:
             continue
         break
+
 
 # Edson Copque - https://linktr.ee/edsoncopque
