aea0a9d39855e351c7bca59216986eda15432153
Edson Copque <ecop@disroot.org>
2025-05-12 11:53:05 -0300

    Create 12_-_response.java

---
+++ sequential_structure/12_-_response.java
@@ -0,0 +1,15 @@
+//#12: Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58.
+class Aula07_Exer12 {
+    public static void main(String[] args) {
+        Scanner my_keyword = new Scanner(System.in);
+
+        System.out.println("Informe sua altura: ");
+        float altura = my_keyword.nextFloat();
+
+        double peso_ideal = ((72.7 * altura) - 58);
+        System.out.println("Seu peso ideal é: " + peso_ideal);
+
+        my_keyword.close();
+
+    }
+}
