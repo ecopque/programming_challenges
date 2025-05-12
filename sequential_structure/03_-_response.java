b9e3f90f9b12a084914ffdb359a9f65e236e1b80
Edson Copque <ecop@disroot.org>
2025-05-12 11:49:53 -0300

    Create 03_-_response.java

---
+++ sequential_structure/03_-_response.java
@@ -0,0 +1,17 @@
+//#3: Faça um Programa que peça dois números e imprima a soma.
+class Aula07_Exer03 {
+    public static void main(String[] args) {
+        Scanner my_keyboard = new Scanner(System.in);
+
+        System.out.println("Informe o primeiro número: ");
+        long primeiro = my_keyboard.nextLong();
+
+        System.out.println("Informe o segundo número: ");
+        long segundo = my_keyboard.nextLong();
+
+        long soma = (primeiro + segundo);
+        System.out.println("A soma dois números é: " + soma);
+
+        my_keyboard.close();
+    }
+}
