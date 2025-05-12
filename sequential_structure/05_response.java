035ed5b7646d560978deb3fdceedfa86d2cfbfff
Edson Copque <ecop@disroot.org>
2025-05-12 11:50:37 -0300

    Create 05_response.java

---
+++ sequential_structure/05_response.java
@@ -0,0 +1,14 @@
+//#5: Faça um Programa que converta metros para centímetros. 
+class Aula07_Exer05 {
+    public static void main(String[] args) {
+        Scanner my_keyword = new Scanner(System.in);
+
+        System.out.println("Entre com a quantidade em metros: ");
+        double metros = my_keyword.nextDouble();
+
+        double conversao_centimetros = (metros * 100);
+        System.out.println("A resposta em centímetros é: " + conversao_centimetros);
+
+        my_keyword.close();
+    }
+}
