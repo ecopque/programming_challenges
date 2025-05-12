16d0db654a425c181121c002bf68f06cdadfb321
Edson Copque <ecop@disroot.org>
2025-05-12 11:49:29 -0300

    Create 02_-_response.java

---
+++ sequential_structure/02_-_response.java
@@ -0,0 +1,11 @@
+class Aula07_Exer02 {
+    public static void main(String[] args) {
+        Scanner my_keyboard = new Scanner(System.in);
+        System.out.println("Informe um número inteiro: ");
+
+        int my_input = my_keyboard.nextInt();
+        System.out.println("O número informado foi: " + my_input);
+
+        my_keyboard.close();
+    }
+}
