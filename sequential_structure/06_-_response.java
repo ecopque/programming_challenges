4eb8fde56e55e18668d2cb924c802b1f117857c3
Edson Copque <ecop@disroot.org>
2025-05-12 11:50:53 -0300

    Create 06_-_response.java

---
+++ sequential_structure/06_-_response.java
@@ -0,0 +1,16 @@
+//#6: Faça um Programa que peça o raio de um círculo, calcule e mostre sua área (A=π×r2).
+class Aula07_Exer06 {
+    public static void main(String[] args) {
+        Scanner my_keyword = new Scanner(System.in);
+
+        System.out.println("Informe o raio do círculo: ");
+        double raio = my_keyword.nextDouble();
+
+        double pi = (3.1416);
+        double calculo = (pi * (raio * raio));
+
+        System.out.println("A área do [raio do círculo] é: " + calculo);
+
+        my_keyword.close();
+    }
+}
