cc07484cd2d97266f5e8e6116b633dd5cd33c1a9
Edson Copque <ecop@disroot.org>
2025-05-12 12:51:39 -0300

    Update 13_-_response.java

--- sequential_structure/13_-_response.java
+++ sequential_structure/13_-_response.java
@@ -1,7 +1,7 @@
 //#13: Tendo como dado de entrada a altura (h) de uma pessoa, construa um algoritmo que calcule seu peso ideal, utilizando as seguintes fórmulas:
 // Para homens: (72.7*h) - 58
 // Para mulheres: (62.1*h) - 44.7
-class Aula07_Exer13 {
+class Exer13 {
     public static void main(String[] args) {
         Scanner my_scan = new Scanner(System.in);
 
