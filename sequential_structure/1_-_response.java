640f3c04c67057b5e8cc55ac25c0a036ccc49dc6
Edson Copque <ecop@disroot.org>
2025-05-12 11:48:05 -0300

    Create 1_-_response.java

---
+++ sequential_structure/1_-_response.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+//#1: Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+class Aula07_Exer01 {
+    public static void main(String[] args) {
+        System.out.println("Alô mundo!");
+    }
+}
