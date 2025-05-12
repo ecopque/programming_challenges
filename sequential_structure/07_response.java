3fe3afe40d6cf88bd98c9c18664598fbdd01bdad
Edson Copque <ecop@disroot.org>
2025-05-12 11:51:08 -0300

    Create 07_response.java

---
+++ sequential_structure/07_response.java
@@ -0,0 +1,16 @@
+//#7: Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. 
+class Aula07_Exer07 {
+    public static void main(String[] args) {
+        Scanner my_keyword = new Scanner(System.in);
+
+        System.out.println("Informe o número: ");
+        double area = my_keyword.nextDouble();
+
+        double calculo = (area * area);
+        double dobro = (calculo * 2);
+
+        System.out.println("A área do quadrado é " + calculo + " e o dobro é " + dobro);
+
+        my_keyword.close();
+    }
+}
