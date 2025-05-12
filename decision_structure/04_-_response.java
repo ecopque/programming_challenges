ac5c2177d22c434f3d2d59a050f2c5137dbfe0a5
Edson Copque <ecop@disroot.org>
2025-05-12 19:24:17 -0300

    Update 04_-_response.java

--- decision_structure/04_-_response.java
+++ decision_structure/04_-_response.java
@@ -1,5 +1,5 @@
 //#4: Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
-class Ex04 {
+class Aula10_Ex04 {
     public static void main(String[] args) {
         Scanner my_keyword = new Scanner(System.in);
 
@@ -9,11 +9,11 @@ class Ex04 {
         if (letra.length() != 1) {
             System.out.println("Digite apenas uma letra.");
         } else if (letra.equalsIgnoreCase("A") ||
-                letra.equalsIgnoreCase("E") ||
-                letra.equalsIgnoreCase("I") ||
-                letra.equalsIgnoreCase("O") ||
-                letra.equalsIgnoreCase("U")) {
-                    System.out.println("Vogal.");
+                    letra.equalsIgnoreCase("E") ||
+                    letra.equalsIgnoreCase("I") ||
+                    letra.equalsIgnoreCase("O") ||
+                    letra.equalsIgnoreCase("U")) {
+                        System.out.println("Vogal.");
             }else {
                 System.out.println("Provavelmente consoante.");
         }
