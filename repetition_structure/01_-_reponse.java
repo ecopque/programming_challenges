f2b4c4a6c481a2879c586bb0de8b0c75e37dc93d
Edson Copque <ecop@disroot.org>
2025-05-22 15:26:37 -0300

    Create 01_-_reponse.java

---
+++ repetition_structure/01_-_reponse.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+//#1: Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido. 
+
+class Exe01 {
+    public static void main(String[] args) {
+        Scanner my_keyword = new Scanner(System.in);
+        double nota;
+
+        do {
+            System.out.println("Informe a nota: ");
+            nota = my_keyword.nextDouble();
+
+            if (nota < 0 || nota > 10) {
+                System.out.println("Informe uma nota válida.");
+            }
+        }
+        while (nota < 0 || nota > 10);
+            System.out.println("Nota válida registrada: " + nota);
+            my_keyword.close();
+        }
+    }
