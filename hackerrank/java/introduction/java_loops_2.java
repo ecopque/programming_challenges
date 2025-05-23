711348937983259b29a18920fa576861af2c77a1
Edson Copque <ecop@disroot.org>
2025-05-23 16:04:28 -0300

    Create java_loops_2.java

---
+++ hackerrank/java/introduction/java_loops_2.java
@@ -0,0 +1,9 @@
+// Explicando o enunciado em português simples
+
+//     Você vai receber várias instruções (queries), onde cada uma contém 3 números: a, b e n.
+//     Para cada instrução, gere uma sequência de n números seguindo essa lógica:
+
+//     Comece com a, e adicione 2^0 * b, depois 2^1 * b, depois 2^2 * b, e assim por diante até 2^(n-1) * b.
+
+//     Mostre todos os n números da sequência na mesma linha, separados por espaço.
+
