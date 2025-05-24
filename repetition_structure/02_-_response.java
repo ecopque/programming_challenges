47f0a9b8e8b9bc41b4a0bbb5ef74f7ca83938f77
Edson Copque <ecop@disroot.org>
2025-05-24 10:43:27 -0300

    Update 02_-_response.java

--- repetition_structure/02_-_response.java
+++ repetition_structure/02_-_response.java
@@ -1,25 +1,24 @@
-class Exe02_5 {
+//#2: Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações. 
+class Exe02_7 {
     public static void main(String[] args) {
         Scanner my_keyword = new Scanner(System.in);
 
-        String nome;
-        String senha;
         boolean bandeira;
 
         do {
-            System.out.println("Informe o nome do usuário: ");
-            nome = my_keyword.nextLine();
+            System.out.println("Informe o nome de usuário: ");
+            String nome = my_keyword.nextLine();
 
-            System.out.println("Informe a senha: ");
-            senha = my_keyword.nextLine();
+            System.out.println("Informe a senha do usuário: ");
+            String senha = my_keyword.nextLine();
 
             if (nome.equals(senha)) {
                 bandeira = false;
-                System.out.println("Informe usuário e senha diferentes.");
+                System.out.println("Usuário e Senha não podem ser iguais.");
             }
             else {
                 bandeira = true;
-                System.out.println("Usuário registrado com sucesso.");
+                System.out.println("Usuário registrado com sucesso!");
             }
         }
         while (!bandeira);
