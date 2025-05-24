2ab7dbf346fbb84a89908de25840df4c21abe95f
Edson Copque <ecop@disroot.org>
2025-05-24 15:37:57 -0300

    Create 05_-_response.java

---
+++ repetition_structure/05_-_response.java
@@ -0,0 +1,31 @@
+//#5: Altere o programa anterior permitindo ao usuário informar as populações e as taxas de crescimento iniciais. Valide a entrada e permita repetir a operação. 
+class Exe05 {
+    public static void main(String[] args) {
+        Scanner my_keyword = new Scanner(System.in);
+
+        double pais_a; //#: 3%.
+        double pais_b; //#: 1.5%.
+        int anos = 0;
+
+        System.out.println("Informe a população do país A: ");
+        pais_a = my_keyword.nextDouble();
+        
+        System.out.println("Informe a população do país B: ");
+        pais_b = my_keyword.nextDouble();
+
+        do {
+            if (pais_a <= pais_b) {
+                anos++;
+                pais_a = (((pais_a / 100) * 3) + pais_a);
+                pais_b = (((pais_b / 100) * 1.5) + pais_b);
+            }
+        }
+        while (pais_a <= pais_b);
+
+        System.out.println("Anos acumulados: " + anos);
+        System.out.printf("População do país A: %.0f\n", pais_a);
+        System.out.printf("População do país B: %.0f\n", pais_b);
+        my_keyword.close();
+        return;
+    }
+}
