0694f733c55e18f81b46ff8110d1bc5100a143ca
Edson Copque <ecop@disroot.org>
2025-05-24 15:52:15 -0300

    Update 05_-_response.java

--- repetition_structure/05_-_response.java
+++ repetition_structure/05_-_response.java
@@ -7,20 +7,29 @@ class Exe05 {
         double pais_b; //#: 1.5%.
         int anos = 0;
 
-        System.out.println("Informe a população do país A: ");
-        pais_a = my_keyword.nextDouble();
-        
-        System.out.println("Informe a população do país B: ");
-        pais_b = my_keyword.nextDouble();
+        do {
+            System.out.println("Informa a população do país A: ");
+            pais_a = my_keyword.nextDouble();
+            if (pais_a <= 0) {
+                System.out.println("Entrada inválida!");
+            }          
+        }
+        while (pais_a <= 0);  
 
         do {
-            if (pais_a <= pais_b) {
-                anos++;
-                pais_a = (((pais_a / 100) * 3) + pais_a);
-                pais_b = (((pais_b / 100) * 1.5) + pais_b);
+            System.out.println("Informe a população do país B: ");
+            pais_b = my_keyword.nextDouble();
+            if (pais_b <= 0) {
+                System.out.println("Entrada inválida.");
             }
         }
-        while (pais_a <= pais_b);
+        while (pais_b <= 0);
+
+        while (pais_a <= pais_b) {
+            anos++;
+            pais_a = (((pais_a / 100) * 3) + pais_a);
+            pais_b = (((pais_b /100) * 1.5) + pais_b);
+        }
 
         System.out.println("Anos acumulados: " + anos);
         System.out.printf("População do país A: %.0f\n", pais_a);
