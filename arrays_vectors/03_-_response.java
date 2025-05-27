a6ee451121d2848666b1c183874ba868d84055c7
Edson Copque <ecop@disroot.org>
2025-05-27 10:43:53 -0300

    Create 03_-_response.java

---
+++ arrays_vectors/03_-_response.java
@@ -0,0 +1,32 @@
+// 3. Criar um vetor A com 15 elementos inteiros. Construir um vetor B de mesmo tipo e tamanho, sendo que cada elemento do vetor B deverá ser o quadrado do respectivo elemento de A, ou seja: B[i] = A[i] * A[I].
+class Aula16_Exe03 {
+    public static void main(String[] args) {
+        Scanner my_keyword = new Scanner(System.in);
+
+        int[] vetor_a = new int[15];
+        int[] vetor_b = new int[vetor_a.length];
+
+        System.out.println("Digite os 15 números inteiros para o Vetor A: ");
+        for (int i = 0; i < vetor_a.length; i++) {
+            System.out.print("Vetor A[" + i + "]: ");
+            vetor_a[i] = my_keyword.nextInt();
+        }
+
+        for (int i = 0; i < vetor_b.length; i++) {
+            vetor_b[i] = (vetor_a[i] * vetor_a[i]);
+        }
+
+        System.out.println("\nVetor A:");
+        for (int i = 0; i < vetor_a.length; i++) {
+            System.out.print(vetor_a[i] + " ");
+        }
+
+        System.out.println("\n\nVetor B (cópia de A): ");
+        for (int i = 0; i < vetor_b.length; i++) {
+            System.out.print(vetor_b[i] + " ");
+        }
+
+        System.out.println();
+        my_keyword.close();
+    }
+}
