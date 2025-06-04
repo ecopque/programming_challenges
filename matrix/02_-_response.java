f7fa24c01342356dc2a33db226e97f67b9a62116
Edson Copque <ecop@disroot.org>
2025-06-04 20:01:04 -0300

    Update 02_-_response.java

--- matrix/02_-_response.java
+++ matrix/02_-_response.java
@@ -1,6 +1,6 @@
 
 // 2. Gere e imprima uma matriz M 10x10 com valores aleatórios entre 0-9. Após isso indique qual é o maior e o menor valor da linha 5 e qual é o maior e o menor valor da coluna 7.
-class Exe02 {
+class Aula18_Exe02 {
     public static void main(String[] args) {
         int[][] matriz = new int[10][10];
         Random randomico = new Random();
@@ -19,12 +19,32 @@ class Exe02 {
             System.out.println();
         }
 
-        int maior_numero = matriz[5][7];
-        int linha = (5 + 1);
-        int coluna = (7 + 1);
+        int maior_linha5 = matriz[4][0];
+        int menor_linha5 = matriz[4][0];
 
-        System.out.println("Maior número: " + maior_numero + 
-                           ", Linha: " + linha + 
-                           ", Coluna: " + coluna);
+        for (int j = 0; j < matriz[4].length; j++) {
+            if (matriz[4][j] > maior_linha5) {
+                maior_linha5 = matriz[4][j];
+            }
+            if (matriz[4][j] < menor_linha5) {
+                menor_linha5 = matriz[4][j];
+            }
+        }
+
+        int maior_coluna7 = matriz[0][6];
+        int menor_coluna7 = matriz[0][6];
+
+        for(int i = 0; i < matriz.length; i++) {
+            if (matriz[i][6] > maior_coluna7) {
+                maior_coluna7 = matriz[i][6];
+            }
+            if (matriz[i][6] < menor_coluna7) {
+                menor_coluna7 = matriz[i][6];
+            }
+        }
+        System.out.println("\nMaior valor da linha 5: " + maior_linha5);
+        System.out.println("Menor valor da linha 5: " + menor_linha5);
+        System.out.println("Maior valor da coluna 7: " + maior_coluna7);
+        System.out.println("Menor valor da coluna 7: " + menor_coluna7);
     }
 }
