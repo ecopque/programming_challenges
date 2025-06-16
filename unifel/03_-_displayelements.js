c0c28d29bfb61abf35ad15e0282844a4a62fd8f5
Edson Copque <ecop@disroot.org>
2025-06-16 20:45:37 -0300

    Create 03_-_displayelements.js

---
+++ unifel/03_-_displayelements.js
@@ -0,0 +1,10 @@
+function displayElements(array) {
+  // escreva seu código aqui
+  for (let i = 0; i < array.length; i++) {
+      console.log(array[i]);
+  }
+}
+
+module.exports = {
+  displayElements
+}
