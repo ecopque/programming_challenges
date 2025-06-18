0927301649fe47fe5a2c4dc8ae1881b365cbe4de
Edson Copque <ecop@disroot.org>
2025-06-18 16:36:15 -0300

    Update and rename 01_-_candrive.js to 01_-_response.js

---
+++ unifel/01_-_response.js
@@ -0,0 +1,12 @@
+function canDrive(age) {
+  // escreva seu código aqui
+  if (age >= 18) {
+      console.log('Você pode dirigir.');
+  } else {
+      console.log('Você não pode dirigir.');
+  }
+}
+
+module.exports = {
+  canDrive
+}
